6b32c62ef0010baf84fec3b743e45c66b670e9c5
guoxiaorui4@xdf.cn <guoxiaorui4@xdf.cn>
2019-02-21 13:37:40 +0800

    ok

--- lv/js/index.js
+++ lv/js/index.js
@@ -262,7 +262,7 @@ $(function () {
 
  function runP1() {
     var i = 0;
-    var str = "一辈子挺长的，要是走散了，打我电话好不：15110279257,愿一切美好终能成为美好\n\r—The boy who couldn't lv u more, from then on";
+    var str = "一辈子挺长的，要是因为气话走散了，打我电话好不：15110279257,愿一切美好终能成为美好\n\r—The boy who couldn't lv u more, from then on";
 
     function typing() {
         var mydiv = document.getElementById("display");
