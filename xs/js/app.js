17ee80c903fee2c21dd80b3cb9dd966b117bcb88
GuoXiaorui <sumkissob@163.com>
2019-04-10 23:41:04 +0800

    ok

--- xs/js/app.js
+++ xs/js/app.js
@@ -1,53 +1,77 @@
 !function(){
     var xs = {
-        '斗罗大陆':'./data/data.json',
-        '星辰变' : './data/xingchenbian/1434458_1435000.json'
+        '斗罗大陆':{
+            bsUrl: './data/douluodalu/',
+            def:'./data/douluodalu/data0.json'
+        },
+        '星辰变' : {
+            bsUrl: './data/xingchenbian/',
+            def:'./data/xingchenbian/data0.json'
+        }
     }
     var skip = [...document.querySelectorAll('.skip input')]
     var pre = [...document.querySelectorAll('.pre')]
     var next = [...document.querySelectorAll('.next')]
     var open = [...document.querySelectorAll('.openmulu')]
     var xsJson = []
+    
     // name: n page: p
     var Search = getAppSearch()
     if (!Search.n || !Search.p) {
         Search = {n:'星辰变',p:0}
        setSearch(Search)
     }
-    var xsKey = decodeURI(Search.n)
-    var _json = Search.d?`./data/${Search.d}.json`:xs[xsKey]
-    fetch(_json).then((res)=>{
-        
-        res.json().then(data=>{
-            console.log(data);
-            xsJson = data
-            var page = data[Search.p]
-            if(page){
-                render(page)
-            } else{
-                document.querySelector('body').innerHTML = '未有此章节';
-                setTimeout(()=>{
-                    window.history.back(-1)
-                },1000)
-            }
-            open.forEach(v=>{
-                v.onclick = function(){
-                        mulu(1,data);
-                        v.isOpen = true
-                }
-            })
-        })
-        // var data = res
-       
-    })
+    var xsKey = decodeURI(Search.n);
+    var dataBaseUrl = xs[xsKey].bsUrl
+    qqOnce(Search)
+    
     
     window.onhashchange = function(){
         var s = getAppSearch();
         if(s.p&&xsJson.length){
-            render(xsJson[s.p]);
-            window.scrollTo(0,0)
-            mulu(0);
+            qqOnce(s)
         }
+    }
+
+    function qqOnce(Search){
+        if(xsJson[Search.p]){
+            render(xsJson[Search.p]);
+            window.scrollTo(0,0)
+                mulu(0);
+                return false;
+        }
+        var xsKey = decodeURI(Search.n)
+        var _json = Search.p == undefined?xs[xsKey].def:`${dataBaseUrl}data${Search.p}.json`
+        fetch(_json).then((res)=>{
+            if(res.status == 404){
+                alert('章节不存在！');
+                window.history.back(-1);
+                return false;
+            }
+            res.json().then(data=>{
+                if(!data || Search.p<0){
+                    alert('章节不存在！');
+                    window.history.back(-1);
+                    return false;
+                }
+                xsJson[Search.p] = data
+                var page = xsJson[Search.p];
+                window.scrollTo(0,0)
+                mulu(0);
+                if(page){
+                    render(page)
+                }
+                open.forEach(v=>{
+                    v.onclick = function(){
+                            mulu(1,data);
+                            v.isOpen = true
+                    }
+                })
+            })
+        }).catch(e=>{
+            alert('章节不存在！');
+            window.history.back(-1)
+        })
     }
 
     function render(data){
@@ -56,7 +80,7 @@
         t.innerText = data.title
         c.innerHTML = data.html
         c.innerHTML = c.innerHTML.replace(new RegExp("&nbsp;&nbsp;&nbsp;&nbsp;","gi"),"")
-        skip.forEach(v=>{v.value = parseInt(Search.p) +1})
+        skip.forEach(v=>{v.value = parseInt(getAppSearch().p) +1})
     }
 
     pre.forEach(v=>{
@@ -110,21 +134,34 @@
         var dom = document.querySelector('.mulu');
         if(!o){
             dom && (dom.className = 'mulu');
-            console.log(000)
             return false;
         }
         if (!dom) {
             dom = document.createElement('DIV');
             dom.className = 'mulu'
-            dom.innerHTML = createMulu(data);
+            // dom.innerHTML = createMulu(data);
             document.querySelector('body').appendChild(dom);
-            dom.querySelector('.close').onclick = function(){
-                mulu(0);
-            }
+            
         }
-        setTimeout(()=>{
-            dom.className = 'mulu show'
-        },0)
+        if(!dom.innerHTML){
+
+            fetch(`${dataBaseUrl}mulu.json`).then((res)=>{
+                res.json().then(data=>{
+                    dom.innerHTML = createMulu(data);
+                    dom.querySelector('.close').onclick = function(){
+                        mulu(0);
+                    }
+                    setTimeout(()=>{
+                        dom.className = 'mulu show'
+                    },0)
+                })
+            })
+        }else{
+            setTimeout(()=>{
+                dom.className = 'mulu show'
+            },0)
+        }
+        
     }
     function createMulu(data){
         console.log(data.length);
